4aa7effd94d466f407bf3ba70bb56965ef0aa6d8
Nerotan <626531215@qq.com>
2017-12-28 00:44:24 +0800

    Update RouteTableDV to 3.0

--- src/RouteTableDV/RouteTableDV.cpp
+++ src/RouteTableDV/RouteTableDV.cpp
@@ -6,21 +6,9 @@ RouteTableDV::RouteTableDV(const char* host_ip) {
     router_ip = host_ip;
 }
 
-// Add ip of neighbor.(without push it into table)
-void RouteTableDV::addNeighborIP(const char* neighbor_ip) {
-    string neighbor = neighbor_ip;
-    auto itor=neighbor_list.begin();
-    for(;itor!= neighbor_list.end(); itor++) {
-        if((*itor) == neighbor) break;
-    }
-    if(itor == neighbor_list.end()) {
-        neighbor_list.push_back(neighbor);
-    }
-}
-
 // Add a route item into table.
-// if time_cost == INF, means ip1->ip2 down
-void RouteTableDV::addRoute(const char* ip1, const char* ip2, int time_cost) {
+// if cost == INF, means ip1->ip2 down
+void RouteTableDV::addRoute(const char* ip1, const char* ip2, int cost) {
     int dst_ip1 = -1, dst_ip2 = -1;
     int next_ip1 = -1, next_ip2 = -1;
     string sip1 = ip1, sip2 = ip2;
@@ -33,12 +21,12 @@ void RouteTableDV::addRoute(const char* ip1, const char* ip2, int time_cost) {
     }
 
     //down ip1->ip2 or ip2->ip1
-    if(time_cost == INF) {
+    if(cost == INF) {
         if(next_ip1 != -1 && DVTable[next_ip1].dst_ip == sip2) {
-            DVTable[next_ip1].time_cost = time_cost;
+            DVTable[next_ip1].cost = cost;
         }
         else if(next_ip2 != -1 && DVTable[next_ip2].dst_ip == sip1)
-            DVTable[next_ip2].time_cost = time_cost;
+            DVTable[next_ip2].cost = cost;
         return;
     }
 
@@ -49,11 +37,11 @@ void RouteTableDV::addRoute(const char* ip1, const char* ip2, int time_cost) {
             struct DVTableItem temp;
             temp.dst_ip = sip1;
             temp.next_ip = router_ip;
-            temp.time_cost = time_cost;
+            temp.cost = cost;
             DVTable.push_back(temp);
             //router->ip1 update
-        } else if(DVTable[dst_ip1].time_cost > time_cost) {
-            DVTable[dst_ip1].time_cost = time_cost;
+        } else if(DVTable[dst_ip1].cost > cost) {
+            DVTable[dst_ip1].cost = cost;
             DVTable[dst_ip1].next_ip = sip2;
         }
         //host->ip2
@@ -63,32 +51,32 @@ void RouteTableDV::addRoute(const char* ip1, const char* ip2, int time_cost) {
             struct DVTableItem temp;
             temp.dst_ip = sip2;
             temp.next_ip = router_ip;
-            temp.time_cost = time_cost;
+            temp.cost = cost;
             DVTable.push_back(temp);
             //router->ip2 update
-        } else if(DVTable[dst_ip2].time_cost > time_cost) {
-            DVTable[dst_ip2].time_cost = time_cost;
+        } else if(DVTable[dst_ip2].cost > cost) {
+            DVTable[dst_ip2].cost = cost;
             DVTable[dst_ip2].next_ip = sip1;
         }
     } else {
         //ip1->ip2 or ip2->ip1 in table
         //router->ip1->ip2 better
-        if(next_ip1 != -1 && DVTable[next_ip1].dst_ip == sip2 && DVTable[next_ip1].time_cost > time_cost) {
-            DVTable[dst_ip1].time_cost = time_cost;
+        if(next_ip1 != -1 && DVTable[next_ip1].dst_ip == sip2 && DVTable[next_ip1].cost > cost) {
+            DVTable[dst_ip1].cost = cost;
             //router->ip2->ip1 better
-        } else if(next_ip2 != -1 && DVTable[next_ip2].dst_ip == sip1 && DVTable[next_ip2].time_cost > time_cost) {
-            DVTable[next_ip2].time_cost = time_cost;
+        } else if(next_ip2 != -1 && DVTable[next_ip2].dst_ip == sip1 && DVTable[next_ip2].cost > cost) {
+            DVTable[next_ip2].cost = cost;
         }
         //router->x->ip1 and router->x->ip2 in table while router->ip1->ip2 or router->ip2->ip1 not
         if(dst_ip1 != -1 && dst_ip2 != -1 && DVTable[next_ip1].dst_ip != sip2 && DVTable[next_ip2].dst_ip != sip1) {
             //router->ip2->ip1 better than router->x->ip1
-            if(DVTable[dst_ip1].time_cost > DVTable[dst_ip2].time_cost + time_cost) {
+            if(DVTable[dst_ip1].cost > DVTable[dst_ip2].cost + cost) {
                 DVTable[dst_ip1].next_ip = sip2;
-                DVTable[dst_ip1].time_cost = time_cost;
+                DVTable[dst_ip1].cost = cost;
                 //router->ip1->ip2 better than router->x->ip2
-            } else if(DVTable[dst_ip2].time_cost > DVTable[dst_ip1].time_cost + time_cost) {
+            } else if(DVTable[dst_ip2].cost > DVTable[dst_ip1].cost + cost) {
                 DVTable[dst_ip2].next_ip = sip1;
-                DVTable[dst_ip2].time_cost = time_cost;
+                DVTable[dst_ip2].cost = cost;
             }
         }
         //router->x->ip1 in table while router->ip2 not
@@ -96,7 +84,7 @@ void RouteTableDV::addRoute(const char* ip1, const char* ip2, int time_cost) {
             struct DVTableItem temp;
             temp.dst_ip = sip2;
             temp.next_ip = sip1;
-            temp.time_cost = time_cost;
+            temp.cost = cost;
             DVTable.push_back(temp);
         }
         //router->x->ip2 in table while router->ip1 not
@@ -104,7 +92,7 @@ void RouteTableDV::addRoute(const char* ip1, const char* ip2, int time_cost) {
             struct DVTableItem temp;
             temp.dst_ip = sip1;
             temp.next_ip = sip2;
-            temp.time_cost = time_cost;
+            temp.cost = cost;
             DVTable.push_back(temp);
         }
     }
@@ -113,16 +101,137 @@ void RouteTableDV::addRoute(const char* ip1, const char* ip2, int time_cost) {
 // Find next ip to the destination ip.
 // next_ip = host_ip while destionation is router's neighbor.
 // next_ip = "" while there is no way to go.
-void RouteTableDV::findNextIP(char next_ip[], const char* dst_ip) {
+void RouteTableDV::findNextIP(char* &next_ip, const char* dst_ip) {
     string sdst_ip = dst_ip;
     for (int i = 0; i < DVTable.size(); ++i)
     {
-        if(DVTable[i].dst_ip == sdst_ip && DVTable[i].time_cost != INF) {
+        if(DVTable[i].dst_ip == sdst_ip && DVTable[i].cost != INF) {
             const char* temp = DVTable[i].next_ip.c_str();
             strcpy(next_ip, temp);
             return;
         }
     }
     strcpy(next_ip, "");
-    next_ip[0] = '\0';
+}
+
+//show route message right now.
+void RouteTableDV::print() {
+    int count = DVTable.size();
+    printf("\n");
+    printf("  Next  Address  |  Goal  Address  | Cost\n");
+    printf("-----------------|-----------------|-----\n");
+    for (int i = 0; i < count; i++) {
+        printf("%17s|%17s|%5d\n", DVTable[i].next_ip.c_str(),
+         DVTable[i].dst_ip.c_str(), DVTable[i].cost);
+    }
+    printf("\n");
+}
+
+//remove one item in route table
+void RouteTableDV::removeRoute(const char* ip1, const char* ip2) {
+    string sip1 = ip1, sip2 = ip2;
+    for (auto itor = DVTable.begin(); itor != DVTable.end(); itor++) {
+        //router->ip1->ip2
+        if((*itor).next_ip == sip1 && (*itor).dst_ip == sip2) {
+            DVTable.erase(itor);
+            return;
+        }
+        //router->ip2->ip1
+        if((*itor).next_ip == sip2 && (*itor).dst_ip == sip1) {
+            DVTable.erase(itor);
+            return;
+        }
+    }
+    //printf("Error: Route item doesn't exist\n");
+}
+
+// result format:"next_ip1;next_ip1;cost1;......;host_ipn;dst_ipn;costn;#"
+string RouteTableDV::encode(vector<struct DVTableItem> table) {
+    char str_table[180];
+    int point = 0;
+    //memset is set memory not give memory.
+    memset(str_table, 0, 180);
+    char str_cost[10];
+
+    if(table.size() == 0) return "";
+    for (int i = 0; i < table.size(); ++i)
+    {
+        strncpy(str_table+point, table[i].next_ip.c_str(), table[i].next_ip.length());
+        point += table[i].next_ip.length();
+        str_table[point++] = ';';
+
+        strncpy(str_table+point, table[i].dst_ip.c_str(), sizeof(table[i].dst_ip));
+        point += table[i].dst_ip.length();
+        str_table[point++] = ';';
+        
+        sprintf(str_cost, "%d", table[i].cost); 
+        string test = str_cost;
+        strncpy(str_table+point, str_cost, test.length());
+        point += test.length();
+        str_table[point++] = ';';
+    }
+    str_table[point++] = '#';
+    str_table[point] = '\0';
+    string result = str_table;
+    return result;
+}
+
+// input format:"next_ip1;dst_ip1;cost1;......;next_ipn;dst_ipn;costn;#"
+vector<struct DVTableItem> RouteTableDV::decode(string str_table) {
+    int point_left = 0, point_right = 0;
+    vector<struct DVTableItem> table;
+    struct DVTableItem tempItem;
+    string temp;
+    int temp_cost;
+
+    point_right = str_table.find(';', point_left);
+    for(;str_table[point_left]!='#';) {
+        temp = str_table.substr(point_left, point_right - point_left);
+        tempItem.next_ip = temp;
+        point_left = point_right + 1;
+        point_right = str_table.find(';', point_left);
+        temp = str_table.substr(point_left, point_right - point_left);
+        tempItem.dst_ip = temp;
+        point_left = point_right + 1;
+        point_right = str_table.find(';', point_left);
+        temp = str_table.substr(point_left, point_right - point_left);
+        //temp = temp + '\0';
+        temp_cost = atoi(temp.c_str());
+        tempItem.cost = temp_cost;
+        table.push_back(tempItem);
+        point_left = point_right + 1;
+        point_right = str_table.find(';', point_left);
+    }
+    return table;
+}
+
+// 获取邻居的路由变化信息, 返回自己到其他路由跳数信息，如果自己到其他路由的跳数没变，则返回“”空字符。
+//change is the DVTable of neighbor. Need to be decode
+//func add ip1=neighbor_ip, ip2=DVTable[i].des_ip, cost=DVTable[i].cost
+string RouteTableDV::routeChangeMessage(char* neighbor_ip, string change) {
+    vector<struct DVTableItem> table = decode(change);
+    string before =  encode(DVTable);
+    for(int i = 0; i < table.size(); i++) {
+        addRoute(neighbor_ip, table[i].dst_ip.c_str(), table[i].cost);
+    }
+    string after = encode(DVTable);
+    if(before == after) return "";
+    else return after;
+}
+
+//连接邻居时更新路由表，同时返回自己到其他路由跳数信息
+//the same as addNeighborIP, just plus addRoute and return DVTable.
+string RouteTableDV::addNeighborIP(char* neighbor_ip, int cost) {
+    string neighbor = neighbor_ip;
+    auto itor=neighbor_list.begin();
+    //TODO: waiting to be simple
+    for(;itor!= neighbor_list.end(); itor++) {
+        if((*itor) == neighbor) break;
+    }
+    if(itor == neighbor_list.end()) {
+        neighbor_list.push_back(neighbor);
+    }
+    addRoute(router_ip.c_str(), neighbor_ip, cost);
+    string temp =  encode(DVTable);
+    return temp;
 }
--- src/RouteTableDV/RouteTableDV.h
+++ src/RouteTableDV/RouteTableDV.h
@@ -7,6 +7,7 @@
 #include <queue>
 #include <string>
 #include <stdio.h>      /* for printf() and fprintf() */
+#include <stdlib.h>     /* for atoi() */ 
 #include <string.h>     /* for memset() */
 #include <signal.h>     /* for signal() and SIGALRM */
 #include <errno.h>      /* for errno */
@@ -17,7 +18,7 @@ struct DVTableItem
 {
   string next_ip;
   string dst_ip;
-  int time_cost;
+  int cost; //Default neighbor cost is 1
 };
 
 /*
@@ -25,22 +26,31 @@ struct DVTableItem
  * 2. listen for table require
  * 3. send table requist to neighbor
  *     do loop(every 30s) 
- *         send DV table to neighbor
+ *        send DV table to neighbor
  *     do loop(every 180s)
  *        delete unreachable neighbor
 */
 class RouteTableDV {
 private:
   string router_ip; // Local router address.
-
   std::vector<string> neighbor_list; //neighbors that directly connect to this router.
-
-  // DVTableItem.next_ip DVTableItem.dst_ip DVTableItem.time_cost
+  // DVTableItem.next_ip DVTableItem.dst_ip DVTableItem.cost
   vector<struct DVTableItem> DVTable;
+  // Encode.
+  string encode(vector<struct DVTableItem> DVTable);
+  // Decode.
+  vector<struct DVTableItem> decode(string table_msg);
+
 public:
   RouteTableDV(const char* host_ip);  // Set the ip of host router.
-  void addNeighborIP(const char* neighbor_ip); // Add ip of neighbor.
-  void addRoute(const char* ip1, const char* ip2, int time_cost); // Add a route.
-  void findNextIP(char next_ip[], const char* dst_ip); // Find next ip to the destination ip.
+  //void addNeighborIP(const char* neighbor_ip); // Add ip of neighbor.
+  void findNextIP(char* &next_ip, const char* dst_ip); // Find next ip to the destination ip.
+  void print(); //show route message right now.
+  void removeRoute(const char* ip1, const char* ip2); //remove one item in route table
+  // 获取邻居的路由变化信息, 返回自己到其他路由跳数信息，如果自己到其他路由的跳数没变，则返回“”空字符。
+  string routeChangeMessage(char* neighbor_ip, string change); 
+  //连接邻居时更新路由表，同时返回自己到其他路由跳数信息
+  //the same as addNeighborIP, just plus addRoute and return DVTable.
+  string addNeighborIP(char* neighbor_ip, int cost); 
 };
 #endif
